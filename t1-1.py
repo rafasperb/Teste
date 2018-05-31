100d07f6f843200bf7a4117d3c50a7df0afa089f
rafasperb <rafasperb@gmail.com>
2018-05-31 03:17:15 -0300

    3 ok

--- t1-1.py
+++ t1-1.py
@@ -55,4 +55,67 @@ print(diaseg(dd,mm,aa))
 # e informa o numero de minutos que se passou entre eles.
 
 def minutos(h1,m1,p1,h2,m2,p2):
-    
+    hs = 0
+    ms = 0
+    hb = 0
+
+    if(h1 > 11 or h2 > 11):
+        return -1
+    if(m1 > 59 or m2 > 59):
+        return -1
+
+    if(p1 == 'pm'):
+        h1 = h1+12
+    if(p2 == 'pm'):
+        h2 = h2+12
+
+    if(p1 == 'am' and p2 == 'am' or p1 == 'pm' and p2 == 'pm'):
+        if(h1 == h2 and m1 == m2):
+            hs=0
+            ms=0
+        if(h1 == h2 and m1 < m2):
+            hs=0
+            ms = m2-m1
+
+        if(h1 == h2 and m1 > m2):
+            hs=23
+            ms = (60-m1)+m2
+
+        if(h1 < h2 and m1 < m2):
+            hs = (h2-h1)
+            ms = m2-m1
+
+        if(h1 > h2 and m1 > m2):
+            ms = (60-m1)+m2
+            hs = (24-h1)+h2
+            hb = hb-1
+
+        if(h1 < h2 and m1 > m2):
+            hs = (h2-h1)
+            ms = (60-m1)+m2
+
+        if(h1 > h2 and m1 < m2):
+            ms = m2-m1
+            hs = (24-h1)+h2
+
+        if(h1 < h2 and m1 == m2):
+            hs = (h2-h1)
+            ms = 0
+
+        if(h1 > h2 and m1 == m2):
+            ms = 0
+            hs = (24-h1)+h2
+
+    if(p1 == 'am' and p2 == 'pm'):
+        if(m1 < m2):
+            ms = m2-m1
+        if(m1 > m2):
+            ms = (60-m1)+m2
+            hb = hb-1
+
+        hs = h2-h1
+    hs = hs + hb
+    minutos = (hs*60) + ms
+    # print(str(hs)+' '+str(ms)+' '+'minutos: '+str(minutos))
+
+print(minutos(13,00,'pm',1,01,'pm'))
