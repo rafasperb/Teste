888035b44929a8aa997995a7bbdf513ad35e4558
Rafael Sperb <31015071+rafasperb@users.noreply.github.com>
2018-06-01 15:47:51 -0300

    pronto

--- tests_t1.py
+++ tests_t1.py
@@ -139,10 +139,164 @@ class Testdiaseg(unittest.TestCase):
 
 class Testminutos(unittest.TestCase):
 
-    def test_horario1_(self):
+    def test_horario1_menor_0(self):
 
-        result = t1.diaseg(0,1,2000)
+        result = t1.minutos(-1,10,'pm',10,10,'pm')
         self.assertEqual(result, -1)
+
+    def test_horario2_menor_0(self):
+
+        result = t1.minutos(10,10,'pm',-1,10,'pm')
+        self.assertEqual(result, -1)
+
+    def test_horario1_maior_11(self):
+
+        result = t1.minutos(12,10,'pm',10,10,'pm')
+        self.assertEqual(result, -1)
+
+    def test_horario2_maior_11(self):
+
+        result = t1.minutos(10,10,'pm',12,10,'pm')
+        self.assertEqual(result, -1)
+
+    def test_minutos1_menor_0(self):
+
+        result = t1.minutos(10,-1,'pm',10,10,'pm')
+        self.assertEqual(result, -1)
+
+    def test_minutos2_menor_0(self):
+
+        result = t1.minutos(10,10,'pm',10,-1,'pm')
+        self.assertEqual(result, -1)
+
+    def test_minutos1_maior_59(self):
+
+        result = t1.minutos(10,60,'pm',10,10,'pm')
+        self.assertEqual(result, -1)
+
+    def test_minutos2_maior_59(self):
+
+        result = t1.minutos(10,10,'pm',10,60,'pm')
+        self.assertEqual(result, -1)
+
+    def test_horario1pm_maior_horario2pm(self):
+
+        result = t1.minutos(10,20,'pm',10,10,'pm')
+        self.assertEqual(result, 1430)
+    
+    def test_horario1pm_menor_horario2pm(self):
+
+        result = t1.minutos(10,20,'pm',10,30,'pm')
+        self.assertEqual(result, 10)
+        
+    def test_horario1am_maior_horario2am(self):
+
+        result = t1.minutos(10,20,'am',10,10,'am')
+        self.assertEqual(result, 1430)
+    
+    def test_horario1am_menor_horario2am(self):
+
+        result = t1.minutos(10,20,'am',10,30,'am')
+        self.assertEqual(result, 10)
+
+    def test_horario1pm_maior_horario2am(self):
+
+        result = t1.minutos(10,20,'pm',10,10,'am')
+        self.assertEqual(result, 710)
+    
+    def test_horario1pm_menor_horario2am(self):
+
+        result = t1.minutos(10,20,'pm',10,30,'am')
+        self.assertEqual(result, 730)          
+
+    def test_horario1am_maior_horario2pm(self):
+
+        result = t1.minutos(10,20,'am',10,10,'pm')
+        self.assertEqual(result, 710)
+    
+    def test_horario1am_menor_horario2pm(self):
+
+        result = t1.minutos(10,20,'am',10,30,'pm')
+        self.assertEqual(result, 730)          
+        
+#########################################################
+
+class TestNextdate(unittest.TestCase):
+
+    def test_dia_menor_1(self):
+
+        result = t1.nextdate(0,1,2000,'True')
+        self.assertEqual(result, -1)
+        
+    def test_dia_maior_1(self):
+
+        result = t1.nextdate(2,1,2000,'True')
+        self.assertEqual(result, '3/1/2000 - Segunda-Feira')
+
+    def test_dia_igual_31(self):
+
+        result = t1.nextdate(31,1,2000,'True')
+        self.assertEqual(result, '1/2/2000 - Terça-Feira')
+
+    def test_dia_maior_31(self):
+
+        result = t1.nextdate(32,1,2000,'True')
+        self.assertEqual(result, -1)
+
+    def test_mes_menor_1(self):
+
+        result = t1.nextdate(1,0,2000,'True')
+        self.assertEqual(result, -1)
+
+    def test_mes_maior_1(self):
+
+        result = t1.nextdate(2,1,2000,'True')
+        self.assertEqual(result, '3/1/2000 - Segunda-Feira')      
+        
+    def test_mes_igual_12(self):
+
+        result = t1.nextdate(1,12,2000,'True')
+        self.assertEqual(result, '2/12/2000 - Sábado')
+
+    def test_mes_maior_12(self):
+
+        result = t1.nextdate(1,13,2000,'True')
+        self.assertEqual(result, -1)
+
+    def test_ano_menor_1900(self):
+
+        result = t1.nextdate(1,1,1899,'True')
+        self.assertEqual(result, -1)
+
+    def test_ano_igual_1900(self):
+
+        result = t1.nextdate(1,1,1900,'True')
+        self.assertEqual(result, '2/1/1900 - Terça-Feira')
+
+    def test_ano_igual_2100(self):
+
+        result = t1.nextdate(1,1,2100,'True')
+        self.assertEqual(result, '2/1/2100 - Sábado')
+
+    def test_ano_maior_2100(self):
+
+        result = t1.nextdate(1,1,2101,'True')
+        self.assertEqual(result, -1)
+
+    def test_true(self):
+
+        result = t1.nextdate(1,1,2100,'True')
+        self.assertEqual(result, '2/1/2100 - Sábado')
+
+    def test_false(self):
+
+        result = t1.nextdate(1,1,2100,'False')
+        self.assertEqual(result, '2/1/2100')
+
+    def test_diaSem(self):
+
+        result = t1.nextdate(1,1,2100,'blablabla')
+        self.assertEqual(result, -1)         
 
 if __name__ == '__main__':
     unittest.main()
